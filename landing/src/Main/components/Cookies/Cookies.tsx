85ab08a7e6939fccca9659218bbfee31c3905f95
Magyari Zoltán <codesage.throttle642@passmail.net>
2025-04-02 06:11:43 +0300

    Some CSS optimizations
    - Footer content alignement
    - Space between the text and the link
    - No outline for buttons with focus

--- landing/src/Main/components/Cookies/Cookies.tsx
+++ landing/src/Main/components/Cookies/Cookies.tsx
@@ -6,8 +6,7 @@ const Cookies = () => (
     <span className="message">
       Cookies help us to deliver best user exprience on
       our website. By using the website, you agree the use
-      of cookies.
-      <a href="#" className="link-to-cookie-usage">
+      of cookies. <a href="#" className="link-to-cookie-usage">
         Find out how we use cookies.
       </a>
     </span>
